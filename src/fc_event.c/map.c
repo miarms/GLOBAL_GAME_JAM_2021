9a9bf6d06b56135e3d605ff0d4f22d13cce09a18
Ludovic Zufferey <ludovic.zufferey@epitech.eu>
2021-01-29 19:06:27 +0100

    Push Parallax Map 1

--- src/fc_event.c/map.c
+++ src/fc_event.c/map.c
@@ -16,6 +16,10 @@ void map(t_window s_window)
         //s_game = init_clock_map(s_game);
         sfRenderWindow_display(s_game.window);
         draw_sprite_map(s_game);
+        s_game.pos_batiment.x = s_game.pos_batiment.x - 0.1;
+        sfSprite_setPosition(s_game.s_batiment, s_game.pos_batiment);
+        s_game.pos_sol.x = s_game.pos_sol.x - 0.3;
+        sfSprite_setPosition(s_game.s_sol, s_game.pos_sol);
         while (sfRenderWindow_pollEvent(s_game.window, &s_game.event)) {
             close_window(s_game.window, s_game.event);
         }
