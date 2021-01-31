d996001a4d9206a36cfd0fb317c7e91c636510af
Mia <mia@localhost.localdomain>
2021-01-31 15:20:48 +0100

    score

--- src/fc_event.c/perso.c
+++ src/fc_event.c/perso.c
@@ -28,52 +28,41 @@ t_game collision_memory(t_game s_game, t_memory s_memory)
     (s_game.posperso.y >= s_game.poshappy.y && s_game.posperso.y <= s_game.poshappy.y + 30)) {
         s_game.check_halo = 1;
         s_game.count_moment_happy = 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
     } else if ((s_game.posperso.x + 40 >= s_game.possad.x - 30 && s_game.posperso.x <= s_game.possad.x + 10) && \
     (s_game.posperso.y >= s_game.possad.y && s_game.posperso.y <= s_game.possad.y + 30)) {
         s_game.check_halo = -1;
-        s_game.sad_moment += 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
+        s_game.count_moment_sad = 1;
     } 
 
     if ((s_game.posperso.x + 40 >= s_memory.poshappy1.x && s_game.posperso.x <= s_memory.poshappy1.x + 50) && \
     (s_game.posperso.y >= s_memory.poshappy1.y && s_game.posperso.y <= s_memory.poshappy1.y + 30)) {
         s_game.check_halo = 1;
-        s_game.happy_moment += 1;
         s_game.count_moment_happy = 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
     } else if ((s_game.posperso.x + 40 >= s_memory.possad1.x - 30 && s_game.posperso.x <= s_memory.possad1.x + 10) && \
     (s_game.posperso.y >= s_memory.possad1.y && s_game.posperso.y <= s_memory.possad1.y + 30)) {
         s_game.check_halo = -1;
-        s_game.sad_moment += 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
+        s_game.count_moment_sad = 1;
     }
     //HAPPY 1 & SAD 1
     if ((s_game.posperso.x + 40 >= s_memory.poshappy1.x && s_game.posperso.x <= s_memory.poshappy1.x + 50) && \
     (s_game.posperso.y >= s_memory.poshappy1.y && s_game.posperso.y <= s_memory.poshappy1.y + 30)) {
         s_game.check_halo = 1;
-        s_game.happy_moment += 1;
         s_game.count_moment_happy = 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
     } else if ((s_game.posperso.x + 40 >= s_memory.possad1.x - 30 && s_game.posperso.x <= s_memory.possad1.x + 10) && \
     (s_game.posperso.y >= s_memory.possad1.y && s_game.posperso.y <= s_memory.possad1.y + 30)) {
         s_game.check_halo = -1;
-        s_game.sad_moment += 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
+        s_game.count_moment_sad = 1;
     }
 
     //HAPPY 2 & SAD2
     if ((s_game.posperso.x + 40 >= s_memory.poshappy1.x && s_game.posperso.x <= s_memory.poshappy1.x + 50) && \
     (s_game.posperso.y >= s_memory.poshappy1.y && s_game.posperso.y <= s_memory.poshappy1.y + 30)) {
         s_game.check_halo = 1;
-        s_game.happy_moment += 1;
         s_game.count_moment_happy = 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
     } else if ((s_game.posperso.x + 40 >= s_memory.possad1.x - 30 && s_game.posperso.x <= s_memory.possad1.x + 10) && \
     (s_game.posperso.y >= s_memory.possad1.y && s_game.posperso.y <= s_memory.possad1.y + 30)) {
         s_game.check_halo = -1;
-        s_game.sad_moment += 1;
-        sfSprite_setPosition(s_game.sperso, s_game.posperso);
+        s_game.count_moment_sad = 1;
     }
     return (s_game);
 }
@@ -86,12 +75,12 @@ t_game event_perso(t_game s_game, t_memory s_memory)
     printf ("Sad : %d\n", s_game.sad_moment);
     if (s_game.count_moment_happy == 1) {
         s_game.happy_moment = s_game.happy_moment + 1;
-        usleep(100000);
+        usleep(55000);
         s_game.count_moment_happy = 0;
     }
     if (s_game.count_moment_sad == 1) {
-        s_game.happy_moment = s_game.sad_moment + 1;
-        usleep(100000);
+        s_game.sad_moment = s_game.sad_moment + 1;
+        usleep(50000);
         s_game.count_moment_sad = 0;
     }
     return (s_game);
